ec76ab9d2bb6af6c0642bae6947846b384add84a
Crowdbotics <team@crowdbotics.com>
2024-03-11 18:15:48 +0000

    Initial commit

---
+++ backend/neilphotoapp_46932/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for neilphotoapp_46932 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "neilphotoapp_46932.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "neilphotoapp-46932",
+  url: "https://neilphotoapp-46932.botics.co",
+  api: "https://neilphotoapp-46932.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
